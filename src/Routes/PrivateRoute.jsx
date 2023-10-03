ab3fa156cd5726d1ec62cefd0ee7fec5dee8fb19
Rabiul Mannan Sajed <rabiulmannansajed@gmail.com>
2023-10-03 22:30:48 +0600

    update added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate} from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
-     
+    const location = useLocation(); // to navigate to the previes page  
     if (loading) {
         return <progress className="progress w-56"></progress>
     }
     if (user) {
         return children;
     }
-    return <Navigate to="/login"></Navigate>
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
 };
 
 export default PrivateRoute;
